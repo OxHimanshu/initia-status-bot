d224134450d398a743ccb83b7f7c72b3158cefd6
phoenix <crytosatoshi@gmail.com>
2024-07-03 18:20:31 +0530

    update procfile

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "7313746775:AAGEmFbfTfpRO6YufQtbHaPq_SsCIDFVKbk"
 bot_user_name = "initiastatusbot"
-URL = "the heroku app link that we will create later"
+URL = "https://main-dayle-students007-f02a0015.koyeb.app"
