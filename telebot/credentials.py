41a5441c4cc8300b635900d97abba5515bbb8508
OxHimanshu <61224737+OxHimanshu@users.noreply.github.com>
2024-07-05 16:45:37 +0530

    Update credentials.py

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = "7313746775:AAGEmFbfTfpRO6YufQtbHaPq_SsCIDFVKbk"
-bot_user_name = "initiastatusbot"
-URL = "https://b069-43-249-187-96.ngrok-free.app"
+bot_token = ""
+bot_user_name = ""
+URL = ""
